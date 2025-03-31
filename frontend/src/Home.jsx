6fef9de3ee80e23a07bf9421b3f45c094531e773
David Lu <david124lu@gmail.com>
2025-03-30 22:30:27 -0700

    add loading message to home

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import axios from "axios";
 import RecentReviews from "./RecentReviews";
 import MostReviewedPlayers from "./MostReviewedPlayers";
@@ -10,41 +10,56 @@ const API_URL = import.meta.env.VITE_BACKEND_URL;
 export default function Home() {
   const [recentPlayers, setRecentPlayers] = useState([]);
   const [mostReviewedPlayers, setMostReviewedPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const getRecentPlayers = async () => {
+  const fetchPlayerData = async () => {
     try {
-      const { data } = await axios.get(`${API_URL}/players/recent`);
-      setRecentPlayers(data);
-    } catch (error) {
-      console.log("Error when fetching recent players");
-    }
-  };
+      setLoading(true);
+      const recentPlayers = await axios.get(`${API_URL}/players/recent`);
+      const mostReviewedPlayers = await axios.get(
+        `${API_URL}/players/most-reviewed`
+      );
 
-  const getMostReviewedPlayers = async () => {
-    try {
-      const { data } = await axios.get(`${API_URL}/players/most-reviewed`);
-      setMostReviewedPlayers(data);
+      setRecentPlayers(recentPlayers.data);
+      setMostReviewedPlayers(mostReviewedPlayers.data);
+
+      setLoading(false);
     } catch (error) {
-      console.log("Error when fetching most reviewed players");
+      if (error.response) {
+        setErrorMessage(error.response.data); // Display server error
+      } else {
+        setErrorMessage("An unexpected error occurred.");
+      }
     }
   };
 
   useEffect(() => {
-    getRecentPlayers();
-    getMostReviewedPlayers();
+    fetchPlayerData();
   }, []);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: { xs: "column", sm: "row" },
-        justifyContent: "center",
-        mt: 2,
-      }}
-    >
-      <RecentReviews recentPlayers={recentPlayers} />
-      <MostReviewedPlayers mostReviewedPlayers={mostReviewedPlayers} />
-    </Box>
+    <>
+      {loading ? (
+        <Box sx={{ mt: "10px", textAlign: "center" }}>
+          <Typography sx={{ color: "white" }}>Loading...</Typography>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            justifyContent: "center",
+            mt: 2,
+          }}
+        >
+          <RecentReviews recentPlayers={recentPlayers} />
+          <MostReviewedPlayers mostReviewedPlayers={mostReviewedPlayers} />
+          <Box sx={{ mt: "10px", textAlign: "center" }}>
+            <Typography sx={{ color: "red" }}>{errorMessage}</Typography>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 }
